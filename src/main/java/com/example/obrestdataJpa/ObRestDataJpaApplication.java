046bf6c36f72d3be1040078d52e2b2f79fe6409e
duvan1026 <duvancastro1026@gmail.com>
2023-03-29 11:01:24 -0500

    update files

---
+++ src/main/java/com/example/obrestdataJpa/ObRestDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obrestdataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObRestDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObRestDataJpaApplication.class, args);
+	}
+
+}
