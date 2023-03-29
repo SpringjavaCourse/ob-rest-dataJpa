2576b7ba7aece15da86b87d33dc3ba5c290f8a1a
duvan1026 <duvancastro1026@gmail.com>
2023-03-29 14:12:54 -0500

    implement CRUD with Spring Boot

--- src/main/java/com/example/obrestdataJpa/BookRepository.java
+++ src/main/java/com/example/obrestdataJpa/BookRepository.java
@@ -1,2 +1,11 @@
-package com.example.obrestdataJpa;public interface BookRepository {
+package com.example.obrestdataJpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> { // JpaRepository, mos proporcionara los emtodos para realizar el crud.
+
+
+
 }
