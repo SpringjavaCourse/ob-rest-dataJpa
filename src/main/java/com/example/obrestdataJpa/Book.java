9acfec7ccd25b8449903839a61e5a46f8a637b15
duvan1026 <duvancastro1026@gmail.com>
2023-03-29 13:23:12 -0500

    create class Book

--- src/main/java/com/example/obrestdataJpa/Book.java
+++ src/main/java/com/example/obrestdataJpa/Book.java
@@ -1,4 +1,93 @@
 package com.example.obrestdataJpa;
 
+import org.springframework.expression.ConstructorResolver;
+
+import java.time.LocalDate;
+
 public class Book {
+
+    // Atributos
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private Double price;
+    private LocalDate releaseDate;
+    private Boolean online;
+
+
+    // Constructor
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, Integer pages, Double price, LocalDate releaseDate, Boolean online) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.releaseDate = releaseDate;
+        this.online = online;
+    }
+
+
+    // Getter and Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
 }
