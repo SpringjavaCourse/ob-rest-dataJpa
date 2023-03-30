6e613ded14e0f0b477c9ff5c0544775eb358b38a
duvan1026 <duvancastro1026@gmail.com>
2023-03-30 15:11:28 -0500

    create and implement first controller REST in JAVA

--- src/main/java/com/example/obrestdataJpa/controller/Hellocontroller.java
+++ src/main/java/com/example/obrestdataJpa/controller/Hellocontroller.java
@@ -1,9 +1,13 @@
 package com.example.obrestdataJpa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hellocontroller {
 
-
+    @GetMapping("/hola")
+    public String holaMundo(){
+        return "Hola Mundo que tal vamos mundo, como va todo?";
+    }
 }
