b4d21cfdd263378a9ccdc83e38f351880e7c505a
duvan1026 <duvancastro1026@gmail.com>
2023-03-30 15:40:39 -0500

    implement arquitecture CRUD of the controller

--- src/main/java/com/example/obrestdataJpa/controller/BookController.java
+++ src/main/java/com/example/obrestdataJpa/controller/BookController.java
@@ -5,6 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+    // Crud sobre la entidad Book
 
+    // Buscar todos los libros
+
+
+    // Buscar un solo libro en BD segun su ID
+
+    // Crear un nuevo libro en BD
+
+    // Actualziar un libro existente en BD
+
+    // Borrar un libro en BD
 
 }
