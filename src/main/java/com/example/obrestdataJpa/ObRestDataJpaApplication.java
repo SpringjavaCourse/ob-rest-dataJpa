265d66e1f1e668bdbdd6df5a2059366f71b96f82
duvan1026 <duvancastro1026@gmail.com>
2023-03-30 15:56:23 -0500

    implement Method finAll() for Book get

--- src/main/java/com/example/obrestdataJpa/ObRestDataJpaApplication.java
+++ src/main/java/com/example/obrestdataJpa/ObRestDataJpaApplication.java
@@ -27,7 +27,7 @@ public class ObRestDataJpaApplication {
 		// Recuperar todos los libros
 		System.out.println("Num libros en base de datos: " + repository.findAll().size());
 		// Borrar un libro
-		repository.deleteById(1L);
+		//repository.deleteById(1L);
 
 		System.out.println("Num libros en base de datos: " + repository.findAll().size());
 	}
--- src/main/java/com/example/obrestdataJpa/controller/BookController.java
+++ src/main/java/com/example/obrestdataJpa/controller/BookController.java
@@ -1,13 +1,37 @@
 package com.example.obrestdataJpa.controller;
 
+import com.example.obrestdataJpa.entities.Book;
+import com.example.obrestdataJpa.repositories.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
 
+    // Atributos
+    private BookRepository bookRepository;
+
+    // Constructores
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
     // Crud sobre la entidad Book
 
     // Buscar todos los libros
+
+    /**
+     * http://localhost:8080/api/books
+     * @return List<Book> Lista de libros
+     */
+    @GetMapping("/api/books")
+    public List<Book> findAll(){
+        // recuperar y devolver los libros de BD
+        return bookRepository.findAll();
+    }
 
 
     // Buscar un solo libro en BD segun su ID
