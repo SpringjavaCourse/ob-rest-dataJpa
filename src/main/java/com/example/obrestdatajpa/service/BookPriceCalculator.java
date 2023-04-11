a7903c2e060b901822adbe10f706d7cf6fe24b0c
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 09:23:44 -0500

    implement example method calculatorprice

--- src/main/java/com/example/obrestdatajpa/service/BookPriceCalculator.java
+++ src/main/java/com/example/obrestdatajpa/service/BookPriceCalculator.java
@@ -1,4 +1,18 @@
 package com.example.obrestdatajpa.service;
 
+import com.example.obrestdatajpa.entities.Book;
+
 public class BookPriceCalculator {
+
+    public double caLculatePrice(Book book){
+        double price = book.getPrice();
+
+        if(book.getPages() > 300){
+            price += 5;
+        }
+
+        price += 2.99; // envio
+        return price;
+
+    }
 }
