9de257c39f0cb0eab1af14474d2bd834e5bc0bdd
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 10:09:18 -0500

    file create

---
+++ src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.obrestdatajpa.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
