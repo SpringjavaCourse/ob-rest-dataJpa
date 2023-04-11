dd218da1cd095a050a4edd8531134de27edc0c59
duvan1026 <duvancastro1026@gmail.com>
2023-04-11 10:53:58 -0500

    implement method to test findAll()

--- src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
+++ src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.obrestdatajpa.controller;
 
+import com.example.obrestdatajpa.entities.Book;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,9 +33,10 @@ class BookControllerTest {
         testRestTemplate = new TestRestTemplate(restTemplateBuilder); // este objeto nos permite utilizar los methodos http
     }
 
+
     @Test
     void hello() {
-        ResponseEntity<String> response =
+        ResponseEntity<String> response = // Respuesta Http
                 testRestTemplate.getForEntity("/hola",String.class);
 
         assertEquals(HttpStatus.OK, response.getStatusCode()); // compara el codigo del estado de la peticion este en OK
@@ -39,6 +44,16 @@ class BookControllerTest {
     }
     @Test
     void findAll() {
+
+        ResponseEntity<Book[]> response =   // Respuesta Http
+                testRestTemplate.getForEntity("/api/books", Book[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode()); // verifica la respuesta sea OK
+        assertEquals(200, response.getStatusCodeValue()); // verifica que el valor de codigo http sea 200
+
+        List<Book> books = Arrays.asList(response.getBody()); // Nos convierte un array normal a arrayList
+        System.out.println(books.size());
+
     }
 
     @Test
